bee7ad7b47602c41c9e5d3a985a0c823ed2de129
sanket <wankhdes29@gmail.com>
2023-02-28 15:01:40 +0530

    starter code

---
+++ src/main/java/com/sanket/ContactMeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sanket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMeBackEndApplication.class, args); 
+	}
+
+}
---
+++ src/main/java/com/sanket/controller/Information.java
@@ -0,0 +1,107 @@
+package com.sanket.controller;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Information {
+	
+	private Integer infoId;
+	
+	private String name;
+	
+	private String email;
+
+	private String project;
+	
+	private String message;
+	
+	
+
+	public Information() {
+		super();
+	}
+
+
+
+	public Information(Integer infoId, String name, String email, String project, String message) {
+		super();
+		this.infoId = infoId;
+		this.name = name;
+		this.email = email;
+		this.project = project;
+		this.message = message;
+	}
+
+
+
+	public Integer getInfoId() {
+		return infoId;
+	}
+
+
+
+	public void setInfoId(Integer infoId) {
+		this.infoId = infoId;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getProject() {
+		return project;
+	}
+
+
+
+	public void setProject(String project) {
+		this.project = project;
+	}
+
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Information [infoId=" + infoId + ", name=" + name + ", email=" + email + ", project=" + project
+				+ ", message=" + message + "]";
+	}
+	
+	
+	
+	
+
+}
